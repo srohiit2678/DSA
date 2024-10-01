074c24f43773551eebec5bccfb1f5f46be0ce977
srohiit2678 <rohitsikhwal99@gmail.com>
2024-10-02 03:57:08 +0530

    Create number.c

---
+++ DSA_IN_C/basic/number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    int i;
+// PRINT 1 TO 10 WHITHOUT + OPRATOR 
+    for(i=1;i<=10;i=i-(-1)){
+        printf("%d ",i);
+		
+    }
+	/*printf("\n");
+	for(i=10;i>=1;i--){
+        printf("%d ",11-i);
+    }
+	*/
+}
