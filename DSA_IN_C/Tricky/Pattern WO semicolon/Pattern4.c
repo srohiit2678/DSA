ff977ef29869df28f7fa62969d27de1147b56899
srohiit2678 <rohitsikhwal99@gmail.com>
2024-10-25 20:55:52 +0530

    Create Pattern4.c

---
+++ DSA_IN_C/Tricky/Pattern WO semicolon/Pattern4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(int i,int j){
+    while(i!=6){
+        if(j=i){}
+        while(j!=0){
+            if(printf("%d ",j--)){}
+        }
+        if(printf("\n",i++)){}
+    }
+}
