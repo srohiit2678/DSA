f505a66e22be0bf87f8835a060e21a4a53b01cc1
srohiit2678 <rohitsikhwal99@gmail.com>
2024-10-25 20:56:05 +0530

    Create Pattern5.c

---
+++ DSA_IN_C/Tricky/Pattern WO semicolon/Pattern5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(int i,int j){
+    while(i!=6){
+        if(j=1){}
+        while(j<=i){
+            if(printf("%d ",j++)){}
+        }
+        if(printf("\n",i++)){}
+    }
+}
