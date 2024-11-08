a3e0e80a3c84c9233398bac459e414734b5fa7b5
srohiit2678 <rohitsikhwal99@gmail.com>
2024-11-08 13:32:57 +0530

    Create Pattern7.c

---
+++ DSA_IN_C/Tricky/Recursion Pattern WO semicolon/Pattern7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int show(int a,int b,int n){
+    if(b<=n){
+if(printf("%d ",a) && a==b && printf("\n") && b++){
+    if(a=6){}
+}
+if(show(--a,b,n)){}
+}
+}
+void main(){
+    if(show(5,1,5)){}
+}
