52b60b68f04f233b3bf15241bc30fc2232656a7c
srohiit2678 <rohitsikhwal99@gmail.com>
2024-11-11 02:10:50 +0530

    Create List1.c

---
+++ DSA_IN_C/ALGORITHEM/LINKED-LIST/List1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+ int data;
+ struct Node *next;
+};
+struct Node *start = NULL;
+
+void main()
+{
+    
+     printf("%d \n",sizeof(struct Node)); // 4 + 4 
+
+     struct Node s;
+     s.data=10;
+     s.next = &s;
+     printf("%d \n",s.data); // 10
+     printf("%d \n",s.next); // add.. of s 
+}
