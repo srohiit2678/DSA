99f534745d104788fca18316628581ca41d68d93
srohiit2678 <rohitsikhwal99@gmail.com>
2024-11-11 02:11:06 +0530

    Create CountingSort.c

---
+++ DSA_IN_C/ALGORITHEM/SORTING/CountingSort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdbool.h>
+void csort(int x[] , int n)
+{
+    int y[n];
+    int max = x[0];
+    int min = x[0];
+    int i;
+    for(i=1;i<n;i++){if(min>x[i])min=x[i];}
+    for(i=0;i<n;i++)x[i] = x[i]- min;
+    for(i=1;i<n;i++){if(max<x[i])max=x[i];}
+    max++;
+    int a[max];
+	for(i=0;i<max;i++)a[i]=0;
+    for(i = 0;i<n;i++)a[x[i]]++;
+    for(i=1;i<max;i++)a[i]+=a[i-1];
+    for(i=n-1;i>=0;i--)y[a[x[i]]-=1]=x[i];
+    for(i=0;i<n;i++)x[i] = y[i]+min;
+}
+
+void display(int x[],int n)
+{
+	for(int i=0;i<n;i++){
+		printf("%d ",x[i]);
+	}
+ printf("\n");
+}
+
+void main(){
+	int x[] = {56,29,35,42,15,-41,75,21};
+	int n=8;
+	csort(x,n);
+	display(x,n);
+}
