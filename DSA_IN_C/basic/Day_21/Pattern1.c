fd4c231a7bbc3ba51a0dae25e4f5b06dfb3a30a2
srohiit2678 <rohitsikhwal99@gmail.com>
2024-11-12 23:12:25 +0530

    Create Pattern1.c

---
+++ DSA_IN_C/basic/Day_21/Pattern1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int show(int a,int b,int n){
+    if(b>=1){
+        if(printf("%d ",a) && a==b && printf("\n") && b--){
+            if(a=0){}
+        }
+        if(show(++a,b,n)){}
+    }
+}
+
+void main(){
+    if(show(1,5,5)){}
+}
