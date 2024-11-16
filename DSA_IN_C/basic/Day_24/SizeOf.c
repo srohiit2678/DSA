13ff4f7e637c4f7647fecd8e8655a1313e499f4b
srohiit2678 <rohitsikhwal99@gmail.com>
2024-11-16 21:05:13 +0530

    Create SizeOf.c

---
+++ DSA_IN_C/basic/Day_24/SizeOf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+    printf("%d\n",sizeof(char));
+    printf("%d\n",sizeof(short));
+    printf("%d\n",sizeof(int));
+    printf("%d\n",sizeof(float));
+    printf("%d\n",sizeof(double));
+    
+}
