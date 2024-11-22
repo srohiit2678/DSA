8b419155a0b149a741fef722ac81af0ec31b6cbc
srohiit2678 <rohitsikhwal99@gmail.com>
2024-11-22 11:39:12 +0530

    Create Pointer.c

---
+++ DSA_IN_C/Tricky/Pointer.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+    // int v = 10;
+    // int *q = &v;
+    int x[]={10,20,30,40,50};
+    int *p = x; //  &x  == x == &x[0] 
+    for(int i=0;i<5;i++){
+    printf("%d\n",*p); // x[i] == *(p+i) == i[x] == *(i+x) == *(x+i) ==*(&x[0]+i)
+    p++; //next address => x+i == &x[0]+i == &x[i] == (p+i)
+    }
+}
