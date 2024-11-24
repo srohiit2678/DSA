bc2f053e326a59ce5bc6ca4b8657bd7d6be3420d
srohiit2678 <rohitsikhwal99@gmail.com>
2024-11-24 19:15:59 +0530

    Update ReverseStringStack.c

--- DSA_IN_C/basic/DAY_38/ReverseStringStack.c
+++ DSA_IN_C/basic/DAY_38/ReverseStringStack.c
@@ -1,36 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
-#define CAP 20
+#define CAP 50
 
 char stack[CAP];
-int top = -1;
+int top =-1;
 
 void push(char);
 char pop();
 bool isEmpty();
+char peek();
 
 void main(){
     char input[CAP];
-    printf("Enter The String : ");
-    scanf("%s",input);
+    printf("Enter any String.");
+    scanf("%[^\n]s",input);
     int i=0;
-    while(input[i]!='\0')   push(input[i++]);
+    while(input[i]!='\0'){
+        push(input[i]);        
+        i++;
+    }
     i=0;
-    while(!isEmpty())   input[i++] = pop();
-
+    while(!isEmpty()){
+        input[i++] = pop();
+    }
     printf("%s",input);
+
 }
 
-void push(char c){
-    stack[++top]=c;
+void push(char x){
+    if(top==CAP-1)return;
+    stack[++top] = x;
 }
+
+
 
 char pop(){
+    if(top==-1)return'\0';
     return stack[top--];
 }
-
+char peek(){
+    if(top==-1)return '\0';
+    return stack[top];
+}
 bool isEmpty(){
     if(top==-1)return true;
     return false;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
