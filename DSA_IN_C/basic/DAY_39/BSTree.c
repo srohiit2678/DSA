e5effb638d52eadd78f19c78378cb5aaa59bef04
srohiit2678 <rohitsikhwal99@gmail.com>
2024-12-19 15:52:47 +0530

    Create BSTree.c

---
+++ DSA_IN_C/basic/DAY_39/BSTree.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Tree{
+int data;
+struct Tree *left;
+struct Tree *right;
+};
+struct Tree *root;
+struct Tree *insert(struct Tree*,int);
+void showAll(struct Tree*);
+void main(){
+    root = insert(root,10);
+    insert(root,5);
+    insert(root,15);
+    insert(root,2);
+    insert(root,1);
+    insert(root,6);
+    insert(root,12);
+    insert(root,20);
+    
+    showAll(root);
+
+}
+
+struct Tree *insert(struct Tree* temp,int x){
+if(temp == NULL){
+    temp = malloc(sizeof(struct Tree));
+    temp->data = x;
+    temp->left = NULL;
+    temp->right = NULL;
+}
+else if(temp->data>x){
+    temp->left = insert(temp->left,x);
+}
+else{
+    temp->right = insert(temp->right,x);
+}
+    return temp;
+}
+
+void showAll(struct Tree* temp){
+    if(temp==NULL)return;
+    // printf("%d ",temp->data);//Pre-Order
+    showAll(temp->left);
+    printf("%d ",temp->data);//In-Order
+    showAll(temp->right);
+    // printf("%d ",temp->data);//Post-Order
+}
