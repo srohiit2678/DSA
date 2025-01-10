9d1238cc81eea7880984524d0c3cf3cbcf51e43c
srohiit2678 <rohitsikhwal99@gmail.com>
2025-01-10 10:53:51 +0530

    Create Gretest_Of_Three_Without_Semicolon.c

---
+++ DSA_IN_C/Tricky/Gretest_Of_Three_Without_Semicolon.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(int x,int y,int z,int l){
+	if(printf("",scanf("%d%d%d",&x,&y,&z),printf("Enter the No. "))){}
+    if(x>y&&x>z&&printf("Greatest is %d",x)){}
+    if(y>x&&y>z&&printf("Greatest is %d",y)){}
+    if(z>x&&z>y&&printf("Greatest is %d",z)){}
+	
+	if(scanf("%d%d%d",&x,&y,&z),l=(x>y)?(x>z)?x:y:z,printf("Enter the No. %d",l)){}
+}
+/*
+keyword in c ->32
+
+data type 8 :
+short int char float double signed unsigned long 
+
+C&F s. 11:
+if else switch case break default for while do  continue goto
+
+4)
+struct union sizeof typedef
+
+2)
+void return 
+
+7)
+auto extern register enum volatile const static
+
+*/
